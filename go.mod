55740a2d12071c0c4464edc5d38873805d5ab38c
Nikita Bikmaev <nikitabikmaev@gmail.com>
2020-07-05 01:37:50 +0300

    secure: update the key generation algorithm

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/xxtea/xxtea-go v0.0.0-20170828040851-35c4b17eecf6
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.28.0
 )
--- pkg/file/secure.go
+++ pkg/file/secure.go
@@ -1,20 +1,30 @@
 package file
 
 import (
-	"crypto/sha1"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
 	uuid "github.com/satori/go.uuid"
 	"github.com/xxtea/xxtea-go/xxtea"
+	"golang.org/x/crypto/pbkdf2"
 )
 
 func (f *File) genKey() error {
 	f.ID = uuid.NewV4()
-	hash := sha1.New()
-	_, err := hash.Write([]byte(f.ID.String()))
+
+	salt := make([]byte, 64)
+	_, err := rand.Read(salt)
 	if err != nil {
 		return err
 	}
-	f.Key = base64.URLEncoding.EncodeToString(hash.Sum(nil))
+	hash := pbkdf2.Key([]byte(f.ID.String()), salt, 10000, 64, sha256.New)
+
+	_, err = rand.Read(salt)
+	if err != nil {
+		return err
+	}
+
+	f.Key = base64.URLEncoding.EncodeToString(salt) + base64.URLEncoding.EncodeToString(hash)
 	return nil
 }
 
