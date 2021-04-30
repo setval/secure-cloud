ceb94da1dc7d4fc657f1e3c17a1f125253f34e10
Nikita <nikitadiscore@gmail.com>
2021-04-30 23:51:07 +0300

    chunks: fix

--- pkg/file/chunk.go
+++ pkg/file/chunk.go
@@ -1,12 +1,7 @@
 package file
 
 import (
-	"bufio"
-	uuid "github.com/satori/go.uuid"
 	"io"
-	"io/ioutil"
-	"os"
-	"path"
 )
 
 const (
@@ -14,24 +9,12 @@ const (
 	TrashChunkSize = 4 * 1024 // increase 4KB after encrypt
 )
 
-func ReaderToChunks(reader io.Reader, dstFile string, w *io.PipeWriter, r *io.PipeReader) error {
-	if w == nil || r == nil {
-		panic("pipe is nil")
-	}
-	if err := os.MkdirAll(dstFile, os.ModePerm); err != nil {
-		return err
-	}
+type cryptFn func([]byte) []byte
 
-	out, err := os.Create(path.Join(dstFile, "meta"))
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
+func ReaderToChunks(r io.Reader, w io.Writer, encryptFn cryptFn) error {
 	for {
-		chunkID := uuid.NewV4().String()
 		b := make([]byte, ChunkSize)
-		_, err := reader.Read(b)
+		_, err := r.Read(b)
 		if err != nil {
 			if err == io.EOF {
 				break
@@ -39,20 +22,7 @@ func ReaderToChunks(reader io.Reader, dstFile string, w *io.PipeWriter, r *io.Pi
 			return err
 		}
 
-		if _, err := w.Write(b); err != nil {
-			return err
-		}
-
-		b = make([]byte, ChunkSize+TrashChunkSize)
-		if _, err := r.Read(b); err != nil {
-			return err
-		}
-
-		if _, err := out.WriteString(chunkID + "\n"); err != nil {
-			return err
-		}
-
-		if err := os.WriteFile(path.Join(dstFile, chunkID), b, os.ModePerm); err != nil {
+		if _, err := w.Write(encryptFn(b)); err != nil {
 			return err
 		}
 	}
@@ -60,34 +30,21 @@ func ReaderToChunks(reader io.Reader, dstFile string, w *io.PipeWriter, r *io.Pi
 	return nil
 }
 
-func ChunksToWriter(srcDir string, w *io.PipeWriter, r *io.PipeReader, output io.Writer) error {
-	metaFile, err := os.Open(path.Join(srcDir, "meta"))
-	if err != nil {
-		return err
-	}
-	defer metaFile.Close()
-
-	scan := bufio.NewScanner(metaFile)
-	for scan.Scan() {
-		b, err := ioutil.ReadFile(path.Join(srcDir, scan.Text()))
+func ChunksToWriter(r io.Reader, w io.Writer, decryptFn cryptFn) error {
+	for {
+		b := make([]byte, ChunkSize+TrashChunkSize)
+		_, err := r.Read(b)
 		if err != nil {
-			return err
-		}
-		if _, err := w.Write(b); err != nil {
-			return err
-		}
-
-		if _, err := r.Read(b); err != nil {
+			if err == io.EOF {
+				break
+			}
 			return err
 		}
 
-		if _, err := output.Write(b[:ChunkSize]); err != nil {
+		if _, err := w.Write(decryptFn(b)); err != nil {
 			return err
 		}
 	}
 
-	if scan.Err() != nil {
-		return scan.Err()
-	}
 	return nil
 }
--- pkg/file/chunk_test.go
+++ pkg/file/chunk_test.go
@@ -1,8 +1,10 @@
 package file_test
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/DiscoreMe/SecureCloud/pkg/file"
-	"io"
+	"github.com/magiconair/properties/assert"
 	"os"
 	"testing"
 )
@@ -11,28 +13,17 @@ func TestChunks(t *testing.T) {
 	os.MkdirAll("testdata/outfile", os.ModePerm)
 	defer os.RemoveAll("testdata/outfile")
 
-	ri, wi := io.Pipe()
-	ro, wo := io.Pipe()
-
-	go func() {
-		// encrypt simulation
-		for {
-			b := make([]byte, file.ChunkSize+file.TrashChunkSize)
-			if _, err := ri.Read(b); err != nil {
-				return
-			}
-			wo.Write(b[:file.ChunkSize])
-		}
-	}()
-
 	testfile, err := os.Open("testdata/out.jpg")
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer testfile.Close()
 
-	if err := file.ReaderToChunks(testfile, "testdata/outfile", wi, ro); err != nil {
+	chunksFile, err := os.Create("testdata/out.chunks")
+	if err != nil {
 		t.Fatal(err)
 	}
+	defer chunksFile.Close()
 
 	output, err := os.Create("testdata/out2.jpg")
 	if err != nil {
@@ -40,7 +31,37 @@ func TestChunks(t *testing.T) {
 	}
 	defer output.Close()
 
-	if err := file.ChunksToWriter("testdata/outfile", wi, ro, output); err != nil {
+	if err := file.ReaderToChunks(testfile, chunksFile, func(bytes []byte) []byte {
+		b := make([]byte, file.TrashChunkSize)
+		bytes = append(bytes, b...)
+		return bytes
+	}); err != nil {
 		t.Fatal(err)
 	}
+
+	chunksFile.Close()
+	chunksFile, _ = os.Open("testdata/out.chunks")
+
+	if err := file.ChunksToWriter(chunksFile, output, func(bytes []byte) []byte {
+		return bytes[:file.ChunkSize]
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	bytes1, err := os.ReadFile("testdata/out.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes2, err := os.ReadFile("testdata/out2.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hash1 := fmt.Sprintf("%x", md5.Sum(bytes1))
+	hash2 := fmt.Sprintf("%x", md5.Sum(bytes2))
+	assert.Equal(t, hash1, hash2)
+	//
+	//	if err := file.ChunksToWriter("testdata/outfile", wi, ro, output); err != nil {
+	//		t.Fatal(err)
+	//	}
 }
